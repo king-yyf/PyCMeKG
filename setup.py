d10b664a3be571d364e46467dcf2fa27982acf30
SigmaHo <sigmayyf@gmail.com>
2019-07-02 16:27:23 +0800

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PyCMeKG",
-    version="0.1.7",
+    version="0.1.8",
     author="Yunfei Yang",
     author_email="yfy@pku.edu.cn",
     description="Chinese Medical Knowledge Graph",
