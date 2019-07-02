bdc01e58934f68700e3ebe2458a1fd8ce60d9b61
SigmaHo <sigmayyf@gmail.com>
2019-07-02 16:06:16 +0800

    modify version

--- pyCMeKG/__init__.py
+++ pyCMeKG/__init__.py
@@ -3,13 +3,45 @@ from pyCMeKG.model import CMeKG
 
 class cmekg(object):
     def __init__(self, db_file = "cmekg_1.0"):
-        self.kg = CMeKG(db_file)
+        with open(db_file, "rb") as f:
+            self.db_dic = pkl.load(f)
 
     def subject(self, subject_name = None):
         """query by subject"""
-        return self.kg.query_by_subject(subject_name)
+        return self.query_by_subject(subject_name)
 
     def subject_predicate(self, subject_name = None, predicate_name = None):
         """query by subject, predicate"""
-        return self.kg.query_by_subject_predicate(subject_name, predicate_name)
+        return self.query_by_subject_predicate(subject_name, predicate_name)
 
+    def query_by_subject(self, subject_name = None):
+        """A list of (subject, predicate, object) tuples for the given subject"""
+
+        dic = {}
+        if subject_name in self.db_dic.keys():
+            dic = self.db_dic[subject_name]
+
+        triples = []
+
+        for key in dic.keys():
+            if isinstance(dic[key], list):
+                for val in dic[key]:
+                    triple = (subject_name, key, val)
+                    triples.append(triple)
+
+        return triples
+
+    def query_by_subject_predicate(self, subject = None, predicate = None):
+        """A list of (subject, predicate, object) tuples for the given subject, predicate"""
+
+        dic = {}
+        if subject in self.db_dic.keys():
+            dic = self.db_dic[subject]
+
+        triples = []
+        if predicate in dic.keys():
+            for val in dic[predicate]:
+                triple = (subject, predicate, val)
+                triples.append(triple)
+
+        return triples
